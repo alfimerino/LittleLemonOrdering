f7e2ea23811580336d3e98ab2aac57e3c0d9a322
Alfredo Merino <alfimerino@gmail.com>
2024-03-10 16:40:41 -0400

    Initial Commit

---
+++ LittleLemonOrdering/LittleLemonOrderingApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonOrderingApp.swift
+//  LittleLemonOrdering
+//
+//  Created by Alfredo Merino on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonOrderingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
